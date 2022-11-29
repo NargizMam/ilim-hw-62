caed77844f50c1163a0ceb907f00e80019027ea5
Nargiz Mamytova <nargiztologonovna@gmail.com>
2022-11-29 16:08:52 +0600

    Удаление неиспользуемых файлов.

---
+++ src/components/NewChild/NewChild.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewChild = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NewChild;
---
+++ src/containers/KinderGarden/KinderGarden.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const KinderGarden = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default KinderGarden;
